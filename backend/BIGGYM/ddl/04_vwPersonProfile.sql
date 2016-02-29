20762cbd2478a12fdaa501d0dd3b3d586fec5aee
captainkirk854 <patrickfraioli@hotmail.com>
2016-02-29 22:25:12 +0000

    changes to view definitions to reflect structural additions

--- backend/BIGGYM/ddl/04_vwPersonProfile.sql
+++ backend/BIGGYM/ddl/04_vwPersonProfile.sql
@@ -9,10 +9,12 @@ use BIGGYM;
 create or replace view vwPersonProfile as
 select
       per.ID PERSONid,
-      prf.ID PROFILEid,
+      prf.ID PERSONPROFILEid,
       per.FIRST_NAME, 
       per.LAST_NAME,
       per.BIRTH_DATE DOB,
+      per.gender,
+      per.body_height,
       round((datediff(curdate(), per.BIRTH_DATE) / 365), 1) AGE,
       prf.NAME PROFILE_NAME,
       prf.DATE_REGISTERED PROFILE_REGISTRATION,
--- backend/BIGGYM/ddl/05_vwPlanProgress.sql
+++ backend/BIGGYM/ddl/05_vwPlanProgress.sql
@@ -8,19 +8,25 @@ use BIGGYM;
 
 create or replace view vwPlanProgress as
 select
+     vwPS.*,
      prg.ID PROGRESSId,
      vwPD.*,
-     prg.DATE_REGISTERED PROGRESS_REGISTRATION,
-     round(datediff(curdate(), prg.SET_DATE), 1) PROGRESS_AGE,
+     prg.DATE_REGISTERED,
+     prg.SET_DATE,
+     round(datediff(curdate(), prg.SET_DATE), 1) SET_AGE,
      prg.SET_REPS,
      prg.SET_WEIGHT,
-     prg.SET_DATE,
-     prg.SET_ORDINALITY
+     prg.SET_ORDINALITY,
+     prg.set_comment COMMENTS,
+     prg.body_weight BODY_WEIGHT
  from
      vwPlanDefinition vwPD,
+     vwPersonProfile vwPS,
      PROGRESS prg 
 where
+     vwPS.PERSONPROFILEid = vwPD.PLANPROFILEid
+  and
      vwPD.DEFINITIONid = prg.DEFINITIONid
     ;
 
-select * from vwPlanProgress order by SET_DATE, SET_ORDINALITY limit 10;
+select * from vwPlanProgress order by SET_DATE, EXERCISE_ORDINALITY, EXERCISE_NAME, BODY_PART, EXERCISE_WEEK, EXERCISE_DAY , SET_ORDINALITY limit 100;
